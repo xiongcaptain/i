72ccff76565fa7dbd086da15868b99df30a4687f
iole <cmlnoah04@gmail.com>
2016-07-29 23:56:52 +0800

    Create u.sh

---
+++ u.sh
@@ -0,0 +1,21 @@
+
+dir=`pwd`
+
+port="53488"
+password="password"
+
+kill(){
+	
+	s = ps -ef | grep server.py | awk '{print $2}'
+	kill ${s}
+}
+
+update(){
+	
+	cd ${dir}/shadowsocks
+	git pull
+	python server.py -p ${port} -k ${password} -m rc4-md5 -o http_simple -d start
+
+}
+
+echo "Finished"
