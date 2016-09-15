befce9e2927864c66ffb01c4038f3987dbb65b36
iole <cmlnoah04@gmail.com>
2016-09-15 15:56:42 +0800

    Update p.sh

--- p.sh
+++ p.sh
@@ -1,6 +1,6 @@
 
 wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
-tar -zxvf Python-2.7.9.tgz
+tar -zvf Python-2.7.9.tgz
 cd Python-2.7.9
 ./configure --prefix=/usr/local/
 make && make install
