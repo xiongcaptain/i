22706d1900c16dee164586feda83eb003c2619ca
iole <cmlnoah04@gmail.com>
2016-09-15 15:45:14 +0800

    Update p.sh

--- p.sh
+++ p.sh
@@ -1,7 +1,7 @@
 
-wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
-tar -zxvf Python-2.7.12.tgz
-cd Python-2.7.12
+wget https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+tar -zxvf Python-2.7.9.tgz
+cd Python-2.7.9
 ./configure --prefix=/usr/local/
 make && make install
 ln -sf /usr/local/bin/python2.7 /usr/bin/python
